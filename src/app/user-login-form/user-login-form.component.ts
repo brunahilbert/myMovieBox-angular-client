4cebce4c9becbd6856bfdee99a89862e6b097e8f
brunahilbert <bruna.hilbertt@gmail.com>
2023-04-25 16:39:57 +0200

    change login-form style

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,14 +1,15 @@
-// src/app/user-registration-form/user-registration-form.component.ts
 import { Component, OnInit, Input } from '@angular/core';
 
 // You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
 
-// This import brings in the API calls we created in 6.2
-import { UserRegistrationService } from '../fetch-api-data.service';
+// This import brings in the API calls
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -19,26 +20,32 @@ export class UserLoginFormComponent implements OnInit {
   @Input() userData = { Username: '', Password: '' };
 
   constructor(
-    public fetchApiData: UserRegistrationService,
+    public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
-  ) {}
+    public snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   // This is the function responsible for sending the form inputs to the backend
   loginUser(): void {
     this.fetchApiData.userLogin(this.userData).subscribe(
       (result) => {
-        // Logic for a successful user registration goes here! (To be implemented)
+        // Logic for a successful user registration goes here
+        // console.log(result);
+        // Save Username and Password in localstorage
+        localStorage.setItem('user', result.user.Username);
+        localStorage.setItem('token', result.token);
         this.dialogRef.close(); // This will close the modal on success!
-        console.log(result);
         this.snackBar.open(result, 'OK', {
           duration: 2000,
         });
+        // Redirects to /movies
+        this.router.navigate(['movies']);
       },
       (result) => {
-        console.log(result);
+        // console.log(result);
         this.snackBar.open(result, 'OK', {
           duration: 2000,
         });
